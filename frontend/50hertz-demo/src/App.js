38e33dc08d1235895e32ac68308ecd2c62f3fb6d
Martin Andreas Wörz <m.woerz@ieservices.de>
2025-03-07 10:02:58 +0100

    D50-3: Improved demo by extending more details

--- frontend/50hertz-demo/src/App.js
+++ frontend/50hertz-demo/src/App.js
@@ -42,12 +42,14 @@ function App() {
 
   return (
     <div className="App">
-      <h1>EMS Status Dashboard</h1>
+      <h1>50hertz Demo EMS Status Dashboard</h1>
       <div className="status-card">
         <p><strong>Aktueller Strompreis:</strong> {status.current_price} ct (€)</p>
         <p><strong>Status:</strong> {status.charging}</p>
         <p><strong>Batteriekapazität:</strong> {status.battery_capacity_kwh} kWh</p>
-        <p><strong>Batteriekapazität:</strong> {status.battery_capacity_percent} %</p>
+        <p><strong>Batteriekapazität (%):</strong> {status.battery_capacity_percent} %</p>
+        <p><strong>Verbrauchsrate der Anlage:</strong> {status.facility_consumption_rate} kWh/s</p>
+        <p><strong>Gesamter Verbrauch:</strong> {status.total_consumption_kwh} kWh</p>
       </div>
       <footer className="footer">Demo für 50hertz</footer>
     </div>
